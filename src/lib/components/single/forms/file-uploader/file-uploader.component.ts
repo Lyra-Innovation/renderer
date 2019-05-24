d6d7ce1d916ef181b7a6c27ee2abac2ef65f90d5
guillemcordoba <guillem.cordoba@gmail.com>
2019-05-24 16:19:21 +0200

    Fixes

--- src/lib/components/single/forms/file-uploader/file-uploader.component.ts
+++ src/lib/components/single/forms/file-uploader/file-uploader.component.ts
@@ -44,6 +44,12 @@ export class FileUploaderComponent extends CustomForm implements OnInit {
   @Input()
   size: number = 25 * 1000 * 1000; // skip files which are >25mb
 
+  @Input()
+  isImage: boolean = true;
+
+  @Input()
+  imageSize: number[] = [150, 150];
+
   @ViewChild('myFilePicker')
   private filePicker: FilePickerDirective;
 
@@ -65,10 +71,50 @@ export class FileUploaderComponent extends CustomForm implements OnInit {
 
   filePick($event) {
     this.file = $event;
-    this.valueChanges.emit(this.file);
+    if (this.isImage) {
+      resizedataURL(
+        this.file.content,
+        this.imageSize[0],
+        this.imageSize[1]
+      ).then(data => {
+        this.valueChanges.emit(data);
+      });
+    } else {
+      this.valueChanges.emit(this.file.content);
+    }
   }
 
   getMaxSize() {
     return this.size / 1000000 + 'MB';
   }
 }
+
+// Takes a data URI and returns the Data URI corresponding to the resized image at the wanted size.
+function resizedataURL(datas, wantedWidth, wantedHeight) {
+  return new Promise(async function(resolve, reject) {
+    // We create an image to receive the Data URI
+    const img = document.createElement('img');
+
+    // When the event "onload" is triggered we can resize the image.
+    img.onload = function() {
+      // We create a canvas and get its context.
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+
+      // We set the dimensions at the wanted size.
+      canvas.width = wantedWidth;
+      canvas.height = wantedHeight;
+
+      // We resize the image with the canvas method drawImage();
+      ctx.drawImage(img, 0, 0, wantedWidth, wantedHeight);
+
+      const dataURI = canvas.toDataURL();
+
+      // This is the return of the Promise
+      resolve(dataURI);
+    };
+
+    // We put the Data URI in the image's src attribute
+    img.src = datas;
+  });
+} // Use it like : var newDataURI = await resizedataURL('yourDataURIHere', 50, 50);
